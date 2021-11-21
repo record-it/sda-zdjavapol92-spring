0d7407597ac570c657b18abec8f88f88357c7b54
cs <siwon.cezary@gmail.com>
2021-11-21 09:33:42 +0100

    day 2 - init

---
+++ src/main/java/pl/sda/springzdjavapol92/SpringZdjavapol92Application.java
@@ -0,0 +1,13 @@
+package pl.sda.springzdjavapol92;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZdjavapol92Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZdjavapol92Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/springzdjavapol92/SpringZdjavapol92ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.springzdjavapol92;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZdjavapol92ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
