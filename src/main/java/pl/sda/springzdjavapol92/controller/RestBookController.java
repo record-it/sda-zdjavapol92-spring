29759d73b194c02254ac875c73ba34dacb6bf08f
cs <siwon.cezary@gmail.com>
2021-11-27 21:06:32 +0100

    day 3 - EntityTodo, TodoRepository

--- src/main/java/pl/sda/springzdjavapol92/controller/RestBookController.java
+++ src/main/java/pl/sda/springzdjavapol92/controller/RestBookController.java
@@ -1,29 +1,23 @@
 package pl.sda.springzdjavapol92.controller;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import pl.sda.springzdjavapol92.model.Book;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/books")
 public class RestBookController {
 
-    @PostMapping("/books")
+    @PostMapping( "")
     public ResponseEntity<Book> addBook(@Valid @RequestBody Book book){
         book.setId(100);
         return ResponseEntity.status(HttpStatus.CREATED).body(book);
     }
 
-    @GetMapping("/books/{id}")
+    @GetMapping( "/{id}")
     public ResponseEntity<Book> readBook(@PathVariable long id){
         Book book = new Book();
         book.setId(id);
@@ -32,7 +26,7 @@ public class RestBookController {
         return ResponseEntity.of(id < 20 ? Optional.of(book) : Optional.empty());
     }
 
-    @PatchMapping("/books/author/{id}")
+    @PatchMapping( "/author/{id}")
     public ResponseEntity<Book> updateBookAuthor(@PathVariable long id, @RequestParam String value){
         System.out.println(id);
         System.out.println(value);
