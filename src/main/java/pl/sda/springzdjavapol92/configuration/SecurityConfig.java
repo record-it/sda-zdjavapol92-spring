b55a6ef681636b4ca086f07332bc86ecbdac55af
cs <siwon.cezary@gmail.com>
2021-12-04 20:52:47 +0100

    day 5 - rest- app - end

--- src/main/java/pl/sda/springzdjavapol92/configuration/SecurityConfig.java
+++ src/main/java/pl/sda/springzdjavapol92/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
@@ -26,7 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/api/**").hasRole("USER")
                 .antMatchers(HttpMethod.PATCH, "/api/**").hasRole("USER")
                 .antMatchers(HttpMethod.GET, "/api/**").permitAll()
-                .and();
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
