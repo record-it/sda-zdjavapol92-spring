dd857efd1f5bfc68a004ecf8773aa1a7ceddbac7
cs <siwon.cezary@gmail.com>
2021-12-04 09:31:57 +0100

    day 5 - init

--- src/main/java/pl/sda/springzdjavapol92/SpringZdjavapol92Application.java
+++ src/main/java/pl/sda/springzdjavapol92/SpringZdjavapol92Application.java
@@ -33,16 +33,6 @@ public class SpringZdjavapol92Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        EntityTodo todo = new EntityTodo();
-//        todo.setPerson("Adam");
-//        todo.setTitle("Test");
-//        todo.setDeadline(LocalDate.of(2021, 12, 12));
-//        todoRepository.save(todo);
-//        todo = new EntityTodo();
-//        todo.setPerson("Ewa");
-//        todo.setTitle("Java");
-//        todo.setDeadline(LocalDate.of(2021, 12, 16));
-//        todoRepository.save(todo);
         bookRepository.save(EntityBook.builder()
                 .author("Bloch")
                 .title("Java")
@@ -60,30 +50,5 @@ public class SpringZdjavapol92Application implements CommandLineRunner {
                 .title("Java for greens")
                 .build());
         bookRepository.flush();
-        System.out.println("Książki danego autora");
-        System.out.println(bookRepository.findByAuthor("Bloch"));
-        System.out.println("Liczba książek danego autora");
-        System.out.println(bookRepository.countByAuthor("Bloch"));
-        System.out.println("Liczba książek danego autora o tytule zaczynającym sie literą J");
-        System.out.println(bookRepository.countByAuthorAndAndTitleStartingWith("Bloch", "J"));
-        System.out.println(bookRepository.getAllTitles());
-        final List<Object[]> result = bookRepository.getIdAndTitles();
-        result.forEach(row -> {
-            System.out.println(row[0] + " " + row[1]);
-        });
-
-        System.out.println("Związek artykułów z autorami");
-        Author author = Author.builder().firstName("Adam").lastName("Nowak").build();
-        Article article = Article.builder().title("Java").authors(Set.of(author)).build();
-
-        articleRepository.save(article);
-        articleRepository.save(
-                Article.builder()
-                        .title("C#")
-                        .authors(Set.of(Author.builder().firstName("Adam").lastName("Nowak").build()))
-                        .build()
-        );
-        System.out.println(articleRepository.findAll());
-        System.out.println(authorRepository.findAll());
     }
 }
