d2abdc017e111025213e1548ff4c4d9816388e56
cs <siwon.cezary@gmail.com>
2021-12-04 15:58:35 +0100

    day 5 - rest- app -init

--- src/main/java/pl/sda/springzdjavapol92/configuration/SecurityConfig.java
+++ src/main/java/pl/sda/springzdjavapol92/configuration/SecurityConfig.java
@@ -1,19 +1,31 @@
 package pl.sda.springzdjavapol92.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
-public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
+        http
+                .httpBasic()
+                .realmName("realm")
+                .and()
+                .csrf()
                 .disable()
                 .headers()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/api/**").hasRole("USER")
+                .antMatchers(HttpMethod.PATCH, "/api/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
                 .and();
     }
 
